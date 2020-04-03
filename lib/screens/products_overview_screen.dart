2a9ed1b7bfec48ccc1d6f097e4b06ecf534fc0e6
Lamiss Messaoudi <younglamiss@gmail.com>
2020-04-03 05:48:54 +0200

    Filtering ALL the screens (not recommended)

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,13 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../widgets/product_grid.dart';
+import '../provider/products_provider.dart';
+
+enum FilterOption { Favorites, All }
 
 class ProductOverviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final productsContainer = Provider.of<Products>(context, listen: false);
+
     return Scaffold(
       appBar: AppBar(
         title: Text("My Shop "),
+        actions: <Widget>[
+          PopupMenuButton(
+            onSelected: (FilterOption selectredValue) {
+              if (selectredValue == FilterOption.Favorites) {
+                productsContainer.showFavoritesOnly();
+              } else {
+                productsContainer.showAll();
+              }
+            },
+            icon: Icon(Icons.more_vert),
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                child: Text('Only Favorites'),
+                value: FilterOption.Favorites,
+              ),
+              PopupMenuItem(
+                child: Text('Show All'),
+                value: FilterOption.All,
+              ),
+            ],
+          )
+        ],
       ),
       body: ProductGrid(),
     );
