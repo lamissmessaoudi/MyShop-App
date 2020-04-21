c1f60da712bbd39f4e8434242d6ef0bd721ed57d
Lamiss Messaoudi <younglamiss@gmail.com>
2020-04-21 20:03:19 +0200

    Storing orders on the web

--- lib/provider/orders.dart
+++ lib/provider/orders.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
 import './cart.dart';
 
 class OrderItem {
@@ -22,16 +26,45 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
 
-  void addOrder(List<CartItem> cartProducts, double total) {
-    _orders.insert(
-      0,
-      OrderItem(
-        id: DateTime.now().toString(),
-        amount: total,
-        products: cartProducts,
-        dateTime: DateTime.now(),
-      ),
+  // void addOrder(List<CartItem> cartProducts, double total) {
+  //   _orders.insert(
+  //     0,
+  //     OrderItem(
+  //       id: DateTime.now().toString(),
+  //       amount: total,
+  //       products: cartProducts,
+  //       dateTime: DateTime.now(),
+  //     ),
+  //   );
+  //   notifyListeners();
+  // }
+
+  Future<void> addOrder(List<CartItem> cartProducts, double total) async {
+    const url = 'https://flutter-update-3d404.firebaseio.com/orders.json';
+    final timeStamp = DateTime.now();
+
+    final response = await http.post(url,
+        body: json.encode({
+          'amount': total,
+          'dateTime': timeStamp.toIso8601String(),
+          'products': cartProducts
+              .map((cp) => {
+                    'id': cp.id,
+                    'title': cp.title,
+                    'quantity': cp.quantity,
+                    'price': cp.price,
+                  })
+              .toList(),
+        }));
+
+    final newOrder = OrderItem(
+      id: json.decode(response.body)['name'],
+      amount: total,
+      products: cartProducts,
+      dateTime: timeStamp,
     );
+
+    _orders.add(newOrder);
     notifyListeners();
   }
 }
