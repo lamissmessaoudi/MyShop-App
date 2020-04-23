e62f7d7899d6802886ac14ba3dd2535900e914a9
Lamiss Messaoudi <younglamiss@gmail.com>
2020-04-23 07:10:08 +0200

    Login users

--- lib/provider/auth.dart
+++ lib/provider/auth.dart
@@ -7,9 +7,10 @@ class Auth with ChangeNotifier {
   DateTime _expiryDate;
   String _userId;
 
-  Future<void> singup(String email, String password) async {
-    const url =
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBUrfyd2s-NhQyrwuzSY239Qn16f460mOg';
+  Future<void> _authnticate(
+      String email, String password, String urlSegment) async {
+    final url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyBUrfyd2s-NhQyrwuzSY239Qn16f460mOg';
     final response = await http.post(
       url,
       body: json.encode(
@@ -21,5 +22,13 @@ class Auth with ChangeNotifier {
       ),
     );
     print(json.decode(response.body));
+  }
+
+  Future<void> singup(String email, String password) async {
+    return _authnticate(email, password, 'signUp');
+  }
+
+  Future<void> login(String email, String password) async {
+    return _authnticate(email, password, 'signInWithPassword');
   }
 }
