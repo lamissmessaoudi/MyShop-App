408024efac129f4bcbec9e819ad61fe8bdbbe077
Lamiss Messaoudi <younglamiss@gmail.com>
2020-04-24 02:21:14 +0200

    Hnadling Auth errors

--- lib/provider/auth.dart
+++ lib/provider/auth.dart
@@ -1,34 +1,45 @@
-import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'dart:convert';
+
+import 'package:flutter/widgets.dart';
+import 'package:http/http.dart' as http;
+
+import '../models/http_exception.dart';
 
 class Auth with ChangeNotifier {
   String _token;
   DateTime _expiryDate;
   String _userId;
 
-  Future<void> _authnticate(
+  Future<void> _authenticate(
       String email, String password, String urlSegment) async {
     final url =
-        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyBUrfyd2s-NhQyrwuzSY239Qn16f460mOg';
-    final response = await http.post(
-      url,
-      body: json.encode(
-        {
-          'email': email,
-          'password': password,
-          'returnSecureToken': true,
-        },
-      ),
-    );
-    print(json.decode(response.body));
+        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/$urlSegment?key=AIzaSyBUrfyd2s-NhQyrwuzSY239Qn16f460mOg';
+    try {
+      final response = await http.post(
+        url,
+        body: json.encode(
+          {
+            'email': email,
+            'password': password,
+            'returnSecureToken': true,
+          },
+        ),
+      );
+      final responseData = json.decode(response.body);
+      print(responseData);
+      if (responseData['error'] != null) {
+        throw HttpException(responseData['error']['message']);
+      }
+    } catch (error) {
+      throw error;
+    }
   }
 
-  Future<void> singup(String email, String password) async {
-    return _authnticate(email, password, 'signUp');
+  Future<void> signup(String email, String password) async {
+    return _authenticate(email, password, 'signupNewUser');
   }
 
   Future<void> login(String email, String password) async {
-    return _authnticate(email, password, 'signInWithPassword');
+    return _authenticate(email, password, 'verifyPassword');
   }
 }
